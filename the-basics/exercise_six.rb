d92778cb23f0e559dc1eddf89d547ce12d8987a3
TheBertOfHall <75818790+TheBertOfHall@users.noreply.github.com>
2021-11-16 16:30:17 -0500

    adding 3 and 6

---
+++ the-basics/exercise_six.rb
@@ -0,0 +1,8 @@
+def square(number)
+  sqr = number * number
+  puts sqr
+  end
+  
+  square(2.5)
+  square(6.9)
+  square(3.53)
--- the-basics/exercise_three.rb
+++ the-basics/exercise_three.rb
@@ -1,1 +1,10 @@
+movies = { :jaws => "1975",
+          :james_bond => "2004",
+          :attack => "2013",
+          :saw => "2001",
+          :classic => "1981"
 
+}
+
+
+movies.each {|key, value| puts "#{value}"}
