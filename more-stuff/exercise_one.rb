a2cf1092e56c5cdc2dd8d4ba20adefa335ed6f15
TheBertOfHall <75818790+TheBertOfHall@users.noreply.github.com>
2021-11-19 13:26:12 -0500

    finished more exercises chapter

--- more-stuff/exercise_one.rb
+++ more-stuff/exercise_one.rb
@@ -1,1 +1,13 @@
+def check(word)
+  if /lab/.match(word)
+    puts "lab is in #{word}"
+  else
+    puts "lab is not in #{word}"
+end
+end
 
+check("laboratory")
+check("experiment")
+check("Pans Labyrinth")
+check("elaborate")
+check("polar bear")
